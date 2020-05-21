59bfd9a1c672b60aacf362027679e61b32ee0022
Jiwon Jung <jungkt0x@naver.com>
2020-05-21 20:48:23 +0900

    slove part7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,9 +7,13 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
     inlines = [ChoiceInline]
-
-
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
