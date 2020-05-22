93f7434ae70202a215495a23c6493bb0f69e6743
Jiwon Jung <jungkt0x@naver.com>
2020-05-22 21:40:19 +0900

    final commit!

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -11,4 +11,7 @@ def month_archive(request, year,month):
     a_list = Article.objects.filter(pub_date__month=month)
     context = {'year': year,'month':month, 'article_list': a_list}
     return render(request, 'news/month_archive.html', context)
-
+def article_detail(request, year,month,pk):
+    a_list = Article.objects.filter(id=pk)
+    context = {'year': year,'month':month, 'article_list': a_list}
+    return render(request, 'news/detail_archive.html', context)
